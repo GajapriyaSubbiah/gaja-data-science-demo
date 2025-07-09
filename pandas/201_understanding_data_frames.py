5d050ddf75e51c2faa9c0fedf0e2ed71981e342d
Gajapriya <gajapriyasubbiah18@gmail.com>
2025-07-09 17:08:29 -0400

    Understanding common functions

--- pandas/201_understanding_data_frames.py
+++ pandas/201_understanding_data_frames.py
@@ -6,3 +6,25 @@ Created on Wed Jul  9 16:14:10 2025
 """
 
 import pandas as pd
+
+transactions = pd.read_excel("grocery_database.xlsx", sheet_name="transactions")
+
+transactions.shape
+transactions.describe()
+transactions.head(5)
+transactions.tail(5)
+transactions.sample(5)
+
+sample = transactions.sample(frac=0.1)
+
+transactions.isna()
+
+
+customer_details = pd.read_excel("grocery_database.xlsx", sheet_name="customer_details")
+
+customer_details.describe()
+customer_details.isna().sum()
+
+customer_details.nlargest(25, "distance_from_store")
+customer_details.nsmallest(25, "distance_from_store")
+customer_details.nunique()
