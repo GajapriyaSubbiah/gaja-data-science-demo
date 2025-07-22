a2754878ee7b3189674af467895f85b717a186cd
Gajapriya <gajapriyasubbiah18@gmail.com>
2025-07-21 22:33:13 -0400

    duplicated data

---
+++ pandas/212_duplicate date.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 21 22:22:06 2025
+
+@author: subbi
+"""
+
+import pandas as pd
+
+my_df = pd.DataFrame({"customer_id":[1,1,2,2,3],
+                      "transaction_id":[101,102,103,103,104]})
+
+my_df.duplicated()
+my_df.duplicated().sum()
+
+my_df["customer_id"].duplicated()
+
+my_df[my_df.duplicated()]
+
+my_df.duplicated(keep = "first")
+
+my_df.duplicated(keep = "last")
+
+my_df.duplicated(keep = False)
+
